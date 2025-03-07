8a43676abc0ac3dabd616e3233d5a0863a679b57
Leonardo <20230070@ricaldone.edu.sv>
2025-03-07 12:12:02 -0600

    crud

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 //express es un servidor que me dara una red
 
 //importo todo lo de la libreria de express
-import express from "express"
+import express from "express";
+import productsRoutes from "./src/routes/products.js";
 
 //crear constante que es igual a la libreria que importe
 const app = express();
+
+//Que acepte datos de json
+app.use(express.json());
+
+//Definir las rutas de las funciones que tendra la pagina web
+app.use("/api/products", productsRoutes);
 
 //exporto la constante para poder usar express en otros archivos
 export default app;

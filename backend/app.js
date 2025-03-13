e2c206e02f79a4a8b896d7196923f2bbf38a3c46
Leonardo Monterrosa <20230070@ricaldone.edu.sv>
2025-03-13 15:32:19 -0600

    v1.0

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@
 //importo todo lo de la libreria de express
 import express from "express";
 import productsRoutes from "./src/routes/products.js";
+import clientsRoutes from "./src/routes/clients.js";
+import branchesRoutes from "./src/routes/branches.js"
 
 //crear constante que es igual a la libreria que importe
 const app = express();
@@ -12,6 +14,8 @@ app.use(express.json());
 
 //Definir las rutas de las funciones que tendra la pagina web
 app.use("/api/products", productsRoutes);
+app.use("/api/clients", clientsRoutes)
+app.use("/api/branches", branchesRoutes)
 
 //exporto la constante para poder usar express en otros archivos
 export default app;
